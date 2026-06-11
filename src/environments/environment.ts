8c87059cc45f26b1fd1502d69afa942c98fe9d83
Jayson Guevarra <jzon_03@yahoo.com>
2026-06-10 20:33:12 -0400

    update

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCVoS-ccLOpLfH9Vlt8JsPkzDs-ZbCMcSo",
+    authDomain: "upstate-pinoy.firebaseapp.com",
+    projectId: "upstate-pinoy",
+    storageBucket: "upstate-pinoy.firebasestorage.app",
+    messagingSenderId: "404453888958",
+    appId: "1:404453888958:web:0dea563bcb2f577b37dfef",
+    measurementId: "G-WVZW49TWY3"
+  }
+};
